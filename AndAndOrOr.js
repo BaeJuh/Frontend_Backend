2f76f0016a157fb465971272231292c5eb180956
Juhwan Bae <106052739+BaeJuh@users.noreply.github.com>
2024-07-02 15:11:50 +0900

    Create AndAndOrOr.js

---
+++ AndAndOrOr.js
@@ -0,0 +1,2 @@
+(true) && (true) && (document.write("- And line <br>")); // and로 연결될 때 false가 나올 때 까지 뒤를 계속 실행
+(false) || (false) || (document.write("- Or line <br>")); // or로 연결될 때 true가 나올 때 까지 뒤를 계속 실행
